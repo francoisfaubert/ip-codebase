7845ac346a584991dbeb89f125ad91abd66dd580
Francois Faubert <francois.faubert@iprospect.com>
2016-03-09 15:34:28 -0500

    added better slug making

--- src/Common/SlugTrait.php
+++ src/Common/SlugTrait.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace IP\Code\Common;
+
+use Strata\Utility\Inflector;
 
 trait SlugTrait {
 
     public function toSlug($string)
     {
-        return strtolower(preg_replace("/\W/i", "", $string));
+        return strtolower(Inflector::slug($string));
     }
 
 }
